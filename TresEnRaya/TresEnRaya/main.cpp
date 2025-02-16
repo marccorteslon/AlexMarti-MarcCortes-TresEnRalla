24c0ad7005f5b44bc1849d82df7259f711546e65
marccorteslon <marc.cortes@enti.cat>
2025-02-16 20:29:32 +0100

    VersiónFinal
    guardado y cargado de partida
    
    Co-Authored-By: AlexAmarti <185908767+AlexAmarti@users.noreply.github.com>

--- TresEnRaya/TresEnRaya/main.cpp
+++ TresEnRaya/TresEnRaya/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <time.h>
 #include <ctime>
-
+#include <string>
 
 char tablero[3][3] = { {' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '} };
 
@@ -51,21 +52,6 @@ bool tableroLleno() {
     return true;
 }
 
-
-
-//int InteligenciaArtificial() {
-//    int fila, columna;
-//    srand(time(0));
-//
-//    do {
-//        fila = rand() % 3;
-//        columna = rand() % 3;
-//    } while (tablero[fila][columna] != ' ');
-//    
-//    tablero[fila][columna] = 'O';
-//
-//}
-
 void IA() {
     int fila, columna;
     srand(time(NULL));
@@ -76,12 +62,73 @@ void IA() {
     tablero[fila][columna] = 'O';
 }
 
+void guardarPartida() {
+    std::string nombreArchivo;
+    std::cout << "Ingresa un nombre para el archivo de guardado: ";
+    std::cin >> nombreArchivo;
+    nombreArchivo += ".tictacsave";
+
+    std::ofstream archivo(nombreArchivo);
+    if (archivo.is_open()) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                archivo << tablero[i][j] << ' ';
+            }
+            archivo << '\n';
+        }
+        archivo.close();
+        std::cout << "Partida guardada como " << nombreArchivo << "\n";
+    }
+    else {
+        std::cout << "Error :(";
+    }
+}
+
+bool cargarPartida() {
+    std::string nombreArchivo;
+    std::cout << "Ingresa el nombre de tu archivo de gurdado: ";
+    std::cin >> nombreArchivo;
+    nombreArchivo += ".tictacsave";
+
+    std::ifstream archivo(nombreArchivo);
+    if (archivo.is_open()) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                archivo >> tablero[i][j];
+            }
+        }
+        archivo.close();
+        std::cout << "Partida cargada  correctamente :D";
+        return true;
+    }
+    else {
+        std::cout << "Error al cargar tu partida :(";
+        return false;
+    }
+}
+
 void jugar() {
     char jugador = 'X';
     int fila, columna;
 
     while (true) {
         imprimirTablero();
+
+        if (jugador == 'X') {
+            std::cout << "Seguir jugando [1]\nGuardar Partida [2]\n";
+            short opcion;
+            std::cin >> opcion;
+
+            if (opcion == 2) {
+                guardarPartida();
+                continue;
+            }
+            else if (opcion != 1) {
+                std::cout << "Error :(";
+                continue;
+            }
+        }
+
         if (jugador == 'X') {
             std::cout << "Le toca a " << jugador << ". Elige casilla [1-3]: ";
             std::cin >> fila >> columna;
@@ -121,6 +168,19 @@ void jugar() {
 
 
 int main() {
+    std::cout << "MENU PRINCIPAL\n\n";
+    std::cout << "Jugar [1]\n";
+    std::cout << "Cargar Partida [2]\n";
+    std::cout << "Salir [3]\n";
+
+    short optionMenu;
+    std::cin >> optionMenu;
+
+    if (optionMenu == 2) {
+        if (!cargarPartida()) {
+            std::cout << "Iniciando nueva Partida\n";
+        }
+    }
     jugar();
     return 0;
 }
